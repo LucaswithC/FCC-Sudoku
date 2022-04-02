6bf5c322cdc8ba168ba80685ad49f084de9580c7
LucaswithC <lucas.kiers@eclipso.at>
2022-04-02 18:45:55 +0200

    Missing Check

--- routes/api.js
+++ routes/api.js
@@ -7,14 +7,14 @@ module.exports = function (app) {
 
   app.route("/api/check").post((req, res) => {
     // Input Check
-    let val = solver.validate(req.body.puzzle);
-    if(val?.error) return res.json(val)
-    
     let puzzle = arrPuzzle(req.body.puzzle) || "";
     let coordinate = req.body.coordinate || "";
     let value = req.body.value || "";
-
     if (!req.body.puzzle || !coordinate || !value) return res.json({ error: "Required field(s) missing" });
+
+    let val = solver.validate(req.body.puzzle);
+    if(val?.error) return res.json(val)
+    
     if (coordinate.length > 2 || !/[a-iA-I]/.test(coordinate[0]) || !/[0-9]/.test(coordinate[1])) return res.json({ error: "Invalid coordinate" });
     if (value > 9 || value < 1) return res.json({ error: "Invalid value" });
 
